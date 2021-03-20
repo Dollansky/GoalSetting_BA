54d102dfffd4be497bc1b34a052789247bc7ffca
Dollansky <49188175+Dollansky@users.noreply.github.com>
2021-03-20 15:57:53 +0100

    Delete dist directory

--- dist/src/onInstallationSetup.js
+++
@@ -1,5 +0,0 @@
-// TODO run a automatic registration request ID for datacolleciton
-chrome.runtime.onInstalled.addListener(function () {
-    var blacklist = ["www.reddit.com", "www.instagram.com", "www.facebook.com", "www.youtube.com", "www.netflix.com", "9gag.com"];
-    chrome.storage.local.set({ blacklist: blacklist });
-});
